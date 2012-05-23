b76b7451bf8e035378a43e136123876452bac16f
Harinadha <harinath.cau@gmail.com>
2012-05-23 13:09:41 +0900

    Checking Initial upload

---
+++ HAL_MPU6050.h
@@ -0,0 +1,61 @@
+//MPU6050 I2C library for ARM STM32F103xx Microcontrollers - Header file has defines
+// to choose I2C peripheral, speed & pins. 
+// 23/05/2012 by Harinadha Reddy Chintalapalli <harinath.ec@gmail.com>
+// Changelog:
+//     2012-05-23 - initial release.
+/* ============================================================================================
+MPU6050 device I2C library code for ARM STM32F103xx is placed under the MIT license
+Copyright (c) 2012 Harinadha Reddy Chintalapalli
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+================================================================================================
+*/
+
+/* Define to prevent recursive inclusion*/
+#ifndef __HAL_MPU6050_H
+#define __HAL_MPU6050_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+  
+/* Includes */
+#include "stm32f10x.h"
+
+/**
+* @addtogroup  MPU6050_I2C_Define
+* @{
+*/
+
+#define MPU6050_I2C                  I2C2
+#define MPU6050_I2C_RCC_Periph       RCC_APB1Periph_I2C2
+#define MPU6050_I2C_Port             GPIOB
+#define MPU6050_I2C_SCL_Pin          GPIO_Pin_10
+#define MPU6050_I2C_SDA_Pin          GPIO_Pin_11
+#define MPU6050_I2C_RCC_Port         RCC_APB2Periph_GPIOB
+#define MPU6050_I2C_Speed            100000 // 100kHz standard mode
+
+/**
+*@}
+*/ /* end of group MPU6050_I2C_Define */ 
+
+
+#endif /* __HAL___MPU6050_H */
+
+/******************* (C) COPYRIGHT 2012 Harinadha Reddy Chintalapalli *****END OF FILE****/
---
+++ README.txt
@@ -0,0 +1,13 @@
+============================================================================
+Note: For More details, please visit: http://harinadha.wordpress.com
+============================================================================
+
+The MPU6050 I2C Device Library provideS simple and intuitive interfaces to MPU6050 I2C device on ARM 32-bit STM32F103xx family of microcontrollers.It has the I2C bit- and byte-level communication for complete coverage of all functionality described by MPU6050 documentation.
+
+The code is written primarily to support the MPU6050 I2C device implementation, but it may be useful to extend it to other I2C devices.
+
+Documentation for each class is created using Doxygen-style comments placed in each class definition file, based on the information available in device's datasheet. This documentation will be available in HTML format on the http://harinadha.wordpress.com website, which also holds helpful information for some problems i faced in implementing this library.
+
+To use the library, See the includes you need for each of these files.
+
+Want a library for a device that isn't up on the repository? Request it, or fork the code and contribute! Better yet, send me a device on a breakout board to test the code during development. No guarantees on how fast I can get it done, but I'd love to make good consistent and well-documented library on STM32F103xx platform.
